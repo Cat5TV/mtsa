e37b7b503c57a2efa100caecd72db92f23476512
Robbie Ferguson <contact@category5.tv>
2016-06-02 15:29:38 -0400

    Decentralize dependency check.

--- flushcache.sh
+++ flushcache.sh
@@ -29,32 +29,8 @@ if [[ $EUID -ne 0 ]]; then
   echo "ERROR: You must be a root" 2>&1
   exit 1
 else
-    
-  # Check for, install, and verify dependencies
-  if [ $(command -v yum) ]; then
-    PACMAN="$(command -v yum) --y install "
-  fi
-  if [ $(command -v apt-get) ]; then
-    PACMAN="$(command -v apt-get) --allow-unauthenticated --yes install "
-  fi
-
-    for i in "${depends[@]}"
-      do
-          echo Testing dependency \"$i\"...
-          if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ] && [ $(rpm -qa 2>/dev/null | grep -c $i) -eq 0 ]; then
-              echo "Not installed. Installing $i..."
-              $PACMAN $i
-              if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ] && [ $(rpm -qa 2>/dev/null | grep -c $i) -eq 0 ]; then
-                echo "Installation of $i failed. Please install it manually and run this script again."
-                exit 1;
-              else
-                echo "$i was successfully installed."
-              fi
-          else
-            echo "$i is installed. Proceeding."
-          fi
-      done
-
+  source ./inc/check-depends.sh
+  
   echo Flushing buffers to disk...
 
   # (move data, modified through FS -> HDD cache) + flush HDD cache
