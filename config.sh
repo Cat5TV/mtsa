4aadcb987b4fd183d5b8d4482a37c13414c3e184
Robbie Ferguson <contact@category5.tv>
2016-06-03 09:12:04 -0400

    Revisions
    Move mtsa version to a system config file instead of user config file. Add a few user config options.

--- config.sh
+++ config.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # Config file. You'll need to edit this to suit your setup.
 
-ver=1.0
+# Minetest game folder, eg., /home/robbie/minetest
+mtgame=
+
+# Minetest mod folder, eg., /home/robbie/.minetest/mods
+mtmod=
+
+# Where to save your backups, eg., /home/robbie/backups
+mtbackup=
+
+# Minetest hard drive, eg., /dev/sda
+mtdev=
