4bd2bf47d36ed9e2545765c94949d7facf04894a
Robbie Ferguson <contact@category5.tv>
2016-12-14 10:35:18 -0500

    Update cleanauth-interact.sh

--- cleanauth-interact.sh
+++ cleanauth-interact.sh
@@ -2,6 +2,7 @@
 # Run this in the world's folder, same location as auth.txt.
 # It finds any users who don't have interact (actually, who have the nointeract priv), and gives you a simple script to purge them from auth.txt
 # Can automate running ./cleanplayers-destroy.sh after this executes, but by default it allows you to first review and edit before running it.
+# The Minetest Server must NOT be running when you use this particular script since it makes changes to your auth.txt file.
 
 # Enter an array of your exeptions (people to ignore). Case sensitive.
 exceptions=(
@@ -12,6 +13,9 @@ exceptions=(
 # 0 = no output, 1 = output
 output=1
 
+# 0 = do not auto-run, 1 = auto-run after completion
+# Suggest you run it without auto-run first, review the script, and decide if you "trust" it before changing setting
+$autorun=0
 
 file=./cleanauth-interact-destroy.sh
 echo '#!/bin/bash' > $file
@@ -21,7 +25,7 @@ for player in `cat auth.txt`; do
    playername=${player%%:*}
    playerdata=${player#*:}
    if [[ $playerdata == *"nointeract"* ]]; then
-     if [[ $output = 1 ]]; then printf "$playername does not have interact "; fi
+     if [[ $output = 1 ]]; then printf "$playername does not haeve interact "; fi
      if [[ "${exceptions[@]}" =~ "${playername}" ]]; then
        if [[ $output = 1 ]]; then echo "but is in the exceptions list."; fi
      else
@@ -39,5 +43,4 @@ echo "rm $file" >> $file
 chmod +x $file
 echo Done. Please read $file and run it if you approve.
 
-# Uncomment this if you want it to run automatically:
-# $file
+if [[ $autorun = 1 ]]; then $file; else echo "Done. Please read $file and run it if you approve."; fi
