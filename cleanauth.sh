7fd336dc5acb6ce04d9acfce1d819f3ef4898559
Robbie Ferguson <contact@category5.tv>
2016-12-14 09:49:33 -0500

    Update cleanauth.sh

--- cleanauth.sh
+++ cleanauth.sh
@@ -23,7 +23,7 @@ done
 # Loop through the player files and see if the player file exists in the array. If not, plan to delete the player file
 for playerfile in "./players/"; do
   echo "Processing $playerfile file..."
-  if [[ ! " ${players[@]} " =~ " ${$playerfile} " ]]; then
+  if [[ ! " ${players[@]} " =~ " ${playerfile} " ]]; then
     echo "rm ./players/$playerfile" >> $file
   fi
 done
