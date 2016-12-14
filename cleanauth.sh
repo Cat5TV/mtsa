31b7326cc2d0d58f31807714a2c27655e4297684
Robbie Ferguson <contact@category5.tv>
2016-12-14 09:18:38 -0500

    Update cleanauth.sh

--- cleanauth.sh
+++ cleanauth.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Run this in the world's folder, same location as auth.txt.
-# It finds any users who don't actually exist, and gives you a simple script to purge them from auth.txt
+# It finds any users who don't actually exist in auth.txt, and gives you a simple script to purge them from the players/ folder
 # Can automate running ./cleanplayers-destroy.sh after this executes, but by default it allows you to first review and edit before running it.
+
+# 0 = no output, 1 = output
+output=1
+
+# 0 = do not auto-run, 1 = auto-run after completion
+# Suggest you run it without auto-run first, review the script, and decide if you "trust" it before changing setting
+$autorun=0
 
 file=./cleanauth-destroy.sh
 echo '#!/bin/bash' > $file
@@ -10,10 +17,10 @@ echo "cp ./auth.txt auth.bak" >> $file
 for player in `cat auth.txt`; do
    playername=${player%%:*}
     if [[ -e "players/$playername" ]]; then
-      echo "$playername exists";
+      if [[ $output = 1 ]]; then echo "$playername exists";
     else
-      echo "$playername does not exist";
-      echo "sed -i '/^$playername:/d' ./auth.txt" >> $file
+      if [[ $output = 1 ]]; then echo "$playername does not exist";
+      echo "rm 'players/$playername'" >> $file
     fi
 
 done
@@ -21,4 +28,5 @@ done
 echo "echo Done. There is a backup auth.bak file, just in case." >> $file
 echo "rm $file" >> $file
 chmod +x $file
-echo Done. Please read $file and run it if you approve.
+
+if [[ $autorun = 1 ]]; then $file; else echo "Done. Please read $file and run it if you approve."; fi
